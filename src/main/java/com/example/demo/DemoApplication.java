f46dd722364279f37767bb68caf391f5698b0f0d
liwansong <615985141@qq.com>
2020-04-15 19:11:20 +0800

    初始化

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,6 @@ public class DemoApplication {
 
 
 	//测试
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
---
+++ src/main/java/com/example/demo/ceshi.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author lws
+ * @date 2020/4/15 - 19:09
+ */
+public class ceshi {
+    public static void main(String[] args) {
+
+    }
+}
